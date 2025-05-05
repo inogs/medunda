d976340f1f5444686075ff8f197bd864f1035222
Stefano Piani <spiani@ogs.it>
2025-05-05 14:39:33 +0200

    Removed useless conversion from string to datetime

--- src/medunda/downloader.py
+++ src/medunda/downloader.py
@@ -9,10 +9,6 @@ from sources.cmems import VARIABLES
 from tools.argparse_utils import date_from_str
 
 
-start=datetime (year= 1999, month=1, day=1)
-end=datetime (year= 2023, month=12, day=31)
-
-
 parameters = {      
      #geographical coordinates and bathymetry 
     "minimum_latitude":41,
@@ -70,16 +66,19 @@ def parse_args ():
     return parser.parse_args()
 
 
-def download_data (variable, output_dir, frequency, start, end):
+def download_data (variable: str, output_dir:str , frequency:str, start:datetime, end:datetime):
 
     """ Download and organize data by year, month and day,  for the chosen variables. 
     Steps: 1) Search in the 'products' dictionary for the product_id related to the chosen variable
            2) Create the output directory if it does not exist
            3) Define the output file name based on the variable
            4) Call copernicusmarine.subset () using the **parameters"""
-    
+
+    if frequency not in ["daily", "monthly"]:
+        raise ValueError(f"invalid frequency")
+
     #1. search for product
-    selected_product=None
+    selected_product = None
     for prod_id, vars_available in PRODUCTS.items():             #zip: to loop between more keys
         if variable in vars_available:
             selected_product=prod_id
@@ -95,13 +94,6 @@ def download_data (variable, output_dir, frequency, start, end):
     #3. define the output file name (exp: monthly.uo_1999-2023.nc)
     output_dir=os.path.join(output_dir, variable, frequency)
     output_filename = os.path.join(output_dir,f"'{frequency}''{variable}'_'{start}'-'{end}'.nc")
-
-
-    if frequency in ["daily", "monthly"]:
-        start_datetime= date_from_str(start)
-        end_datetime= date_from_str(end)
-    else:
-        raise ValueError(f"invalid frequency")
 
     print (f"downloading '{frequency}''{variable}' from '{start}' to '{end}'")
 
@@ -153,7 +145,8 @@ def validate_dataset(filepath, variable):
 
 def main ():
     args=parse_args()       #parse the command line arguments
-    download_data(args.variable, args.output_dir, args.frequency, args.start_date, args.end_date)     
+
+    download_data(args.variable, args.output_dir, args.frequency, args.start_date, args.end_date)
 
     filepath = os.path.join(args.output_dir, f"{args.variable}'{args.frequency}'_'{args.start}'-'{args.end}'.nc")
 
