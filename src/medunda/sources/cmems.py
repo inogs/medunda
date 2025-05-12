1f3e72103203af9fedb546860fae0f686895272b
Stefano Piani <spiani@ogs.it>
2025-05-12 14:32:05 +0200

    Changing products into a dictionary (adding frequency enum)

--- src/medunda/sources/cmems.py
+++ src/medunda/sources/cmems.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # PRODUCTS = {     
 #        # -> each key represents a product and its associated with a list of available variables.
 #      # physical variables (MEDSEA_MULTIYEAR_PHY_006_004)
@@ -12,15 +14,22 @@
 #     "med-ogs-pft-rean-m": ["chl"] ,            # chlorophylle a
 # }
 
-PRODUCTS={
-    ("thetao",): ["med-cmcc-tem-rean-d", "med-cmcc-tem-rean-m"],
-    ("vo", "uo"): ["med-cmcc-cur-rean-d", "med-cmcc-cur-rean-m"],
-    ("so",): ["med-cmcc-cur-rean-d", "med-cmcc-sal-rean-m"],
 
-    ("pH",): ["med-ogs-car-rean-d", "med-ogs-car-rean-m"],
-    ("no3","po4","si"): ["med-ogs-nut-rean-d", "med-ogs-nut-rean-m"],
-    ("chl",): ["med-ogs-pft-rean-d", "med-ogs-pft-rean-m"],
-    ("o2",): ["med-ogs-bio-rean-d", "med-ogs-bio-rean-m"]
+class Frequency(Enum):
+    DAILY = 0
+    MONTHLY = 1
+
+
+
+PRODUCTS={
+    ("thetao",): {Frequency.DAILY: "med-cmcc-tem-rean-d", Frequency.MONTHLY: "med-cmcc-tem-rean-m"},
+    ("vo", "uo"): {Frequency.DAILY: "med-cmcc-cur-rean-d", Frequency.MONTHLY:"med-cmcc-cur-rean-m"},
+    ("so",): {Frequency.DAILY: "med-cmcc-cur-rean-d", Frequency.MONTHLY: "med-cmcc-sal-rean-m"},
+
+    ("pH",): {Frequency.DAILY: "med-ogs-car-rean-d", Frequency.MONTHLY: "med-ogs-car-rean-m"},
+    ("no3","po4","si"): {Frequency.DAILY: "med-ogs-nut-rean-d", Frequency.MONTHLY: "med-ogs-nut-rean-m"},
+    ("chl",): {Frequency.DAILY: "med-ogs-pft-rean-d", Frequency.MONTHLY: "med-ogs-pft-rean-m"},
+    ("o2",): {Frequency.DAILY: "med-ogs-bio-rean-d", Frequency.MONTHLY: "med-ogs-bio-rean-m"}
 }
 
 VARIABLES = []
@@ -33,15 +42,14 @@ def search_for_product(var_name: str, frequency:str) -> str:
     """ Given the name of a variable and a frequency, return the name of the CMEMS product that
     contains such variable with the specified frequency."""
 
-    frequency_index = {"daily":0, "monthly":1}
+    frequency_index = {"daily": Frequency.DAILY, "monthly": Frequency.MONTHLY}
     if frequency not in frequency_index:
         raise ValueError ("invalid frequency; use 'daily' or 'monthly'.")
-    freq_index = frequency_index[frequency]
 
     selected_product = None
-    for vars_tuple, prod_list in PRODUCTS.items():    
+    for vars_tuple, prod_dict in PRODUCTS.items():    
         if var_name in vars_tuple:
-            selected_product = prod_list[freq_index]
+            selected_product = prod_dict[frequency_index]
             break
 
     if selected_product is None:
