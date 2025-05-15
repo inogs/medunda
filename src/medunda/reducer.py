eec411b6058a0cbf01f0b58802617da4f169c71c
Nada Akkari <akkarinada8@gmail.com>
2025-05-15 16:19:06 +0200

    Fixed the extract_bottom function and added extract_layer as new function

--- src/medunda/reducer.py
+++ src/medunda/reducer.py
@@ -3,6 +3,8 @@ import argparse
 import logging
 from pathlib import Path
 import xarray as xr
+import numpy as np
+
 
 LOGGER = logging.getLogger()
 
@@ -34,7 +36,7 @@ def compute_average(input_file, output_file):
     LOGGER.info("done")
 
 
-def extract_bottom (input_file, output_file):
+def extract_layer (input_file, output_file):
     LOGGER.info(f"reading the file: {input_file}")
     with xr.open_dataset(input_file) as ds :
         #print(ds['depth'].values)
@@ -44,7 +46,35 @@ def extract_bottom (input_file, output_file):
     LOGGER.info("done")
     print(bottom_layer.coords)
 
- 
+
+def extract_bottom (input_file, output_file):
+    LOGGER.info(f"reading the file: {input_file}")
+    with xr.open_dataset(input_file) as ds :
+        var_name=list(ds.data_vars)[0]
+        mask=ds[var_name].to_masked_array(copy=False).mask
+        fixed_time_mask=mask[0,:,:,:]
+        index_map=np.count_nonzero(~fixed_time_mask, axis=0)
+        
+        current_data = ds[var_name]
+
+        new_shape = current_data.shape[:1] + current_data.shape[2:]
+
+        new_data_array = np.empty(shape=new_shape, dtype=current_data.dtype)
+
+        for i in range (current_data.shape[2]):
+            for j in range (current_data.shape[3]):
+                blue_cells = index_map [i,j]
+            
+
+                current_value = current_data[:, blue_cells-1, i , j ]
+                new_data_array[:, i, j] = current_value
+
+
+        new_data = xr.DataArray(dims= ["time", "latitude", "longitude"], data=new_data_array) 
+        ds[var_name] = new_data
+        
+        ds.to_netcdf(output_file)
+
 def extract_surface (input_file, output_file):
     LOGGER.info(f"reading the file: {input_file}")
     with xr.open_dataset(input_file) as ds:
@@ -77,7 +107,7 @@ def parse_args ():
     parser.add_argument(
         "--action",
         type=str,
-        choices=("average", "extract_bottom", "extract_surface"),
+        choices=("average", "extract_bottom", "extract_surface", "extract_layer"),
         required=True,
         help="End date of the download"
     )
@@ -95,10 +125,12 @@ def main ():
     match action:
         case "average":
             compute_average(input_file, output_file)
-        case "extract_bottom":
-            extract_bottom(input_file, output_file)
+        case "extract_layer":
+            extract_layer(input_file, output_file)
         case "extract_surface":
             extract_surface(input_file, output_file)
+        case "extract_bottom":
+            extract_bottom(input_file, output_file)
         case _:
             raise ValueError(
                 f"Unexpected action: {action}"
