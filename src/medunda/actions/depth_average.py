4dd7e9bb3ef84b556d823bc02f65365b9e1e27f2
Nada Akkari <akkarinada8@gmail.com>
2025-07-13 23:56:31 +0200

    Updated extraction actions with the reducer's interface using the datasets

--- src/medunda/actions/depth_average.py
+++ src/medunda/actions/depth_average.py
@@ -16,24 +16,24 @@ def configure_parser(subparsers):
     )
 
 
-def compute_depth_average(input_file, output_file):
-    LOGGER.info(f"reading file:{input_file}")
-    with xr.open_dataset(input_file) as ds:
-        LOGGER.debug("computing_layer_height")
-        layer_height = compute_layer_height(ds.depth.values)
-        layer_height_extended = xr.DataArray(layer_height, dims=["depth"])
+def compute_depth_average(data: xr.Dataset, output_file):
+    LOGGER.info(f"reading file:{data}")
+    
+    LOGGER.debug("computing_layer_height")
+    layer_height = compute_layer_height(data.depth.values)
+    layer_height_extended = xr.DataArray(layer_height, dims=["depth"])
 
-        var_name = list(ds.data_vars)[0]
-        mask = ds[var_name].to_masked_array(copy=False).mask[0, :, :, :]
-        mask_extended = xr.DataArray(
-            mask,
-            dims=("depth", "latitude", "longitude")
-        )
-        total_height = (layer_height_extended * ~mask_extended).sum(dim="depth")
+    var_name = list(data.data_vars)[0]
+    mask = data[var_name].to_masked_array(copy=False).mask[0, :, :, :]
+    mask_extended = xr.DataArray(
+        mask,
+        dims=("depth", "latitude", "longitude")
+    )
+    total_height = (layer_height_extended * ~mask_extended).sum(dim="depth")
 
-        mean_layer = (ds * layer_height_extended).sum(dim="depth",
+    mean_layer = (data * layer_height_extended).sum(dim="depth",
                                                       skipna=True) / total_height
 
-        LOGGER.info(f"writing file: {output_file}")
-        mean_layer.to_netcdf(output_file)
+    LOGGER.info(f"writing file: {output_file}")
+    mean_layer.to_netcdf(output_file)
     LOGGER.info("done")
--- src/medunda/actions/extract_bottom.py
+++ src/medunda/actions/extract_bottom.py
@@ -16,29 +16,29 @@ def configure_parser(subparsers):
     )
 
 
-def extract_bottom(input_file: Path, output_file: Path):
-    LOGGER.info(f"reading the file: {input_file}")
-    with xr.open_dataset(input_file) as ds:
-        var_name = list(ds.data_vars)[0]
-        mask = ds[var_name].to_masked_array(copy=False).mask
-        fixed_time_mask = mask[0, :, :, :]
-        index_map = np.count_nonzero(~fixed_time_mask, axis=0)
+def extract_bottom(data: xr.Dataset, output_file: Path):
+    LOGGER.info(f"reading the file: {data}")
+    
+    var_name = list(data.data_vars)[0]
+    mask = data[var_name].to_masked_array(copy=False).mask
+    fixed_time_mask = mask[0, :, :, :]
+    index_map = np.count_nonzero(~fixed_time_mask, axis=0)
 
-        current_data = ds[var_name]
+    current_data = data[var_name]
 
-        new_shape = current_data.shape[:1] + current_data.shape[2:]
+    new_shape = current_data.shape[:1] + current_data.shape[2:]
 
-        new_data_array = np.empty(shape=new_shape, dtype=current_data.dtype)
+    new_data_array = np.empty(shape=new_shape, dtype=current_data.dtype)
 
-        for i in range(current_data.shape[2]):
-            for j in range(current_data.shape[3]):
-                blue_cells = index_map[i, j]
+    for i in range(current_data.shape[2]):
+        for j in range(current_data.shape[3]):
+            blue_cells = index_map[i, j]
 
-                current_value = current_data[:, blue_cells - 1, i, j]
-                new_data_array[:, i, j] = current_value
+            current_value = current_data[:, blue_cells - 1, i, j]
+            new_data_array[:, i, j] = current_value
 
         new_data = xr.DataArray(dims=["time", "latitude", "longitude"],
                                 data=new_data_array)
-        ds[var_name] = new_data
+        data[var_name] = new_data
 
-        ds.to_netcdf(output_file)
+        data.to_netcdf(output_file)
