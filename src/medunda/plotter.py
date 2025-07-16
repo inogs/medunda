ee3bbe780062f8e80e5a768b07accc04e8a9a627
Nada Akkari <akkarinada8@gmail.com>
2025-07-16 15:56:51 +0200

    Refactor: separate each plot mode into its own module.
    Previously, all plotting logic was implemented within a single module: plotter.py.
    With this commit, each plot has been moved into its own module under the plots directory.
    Each plotting function now resides in its own file and handles its specific logic independently.
    Additionally, with this commit, the integral computing function has been moved in a separate action alongside those handled by the reducer.

--- src/medunda/plotter.py
+++ src/medunda/plotter.py
@@ -7,11 +7,13 @@ import cmocean
 import numpy as np
 import xarray as xr
 
-
+from medunda.plots import maps
+from medunda.plots import timeseries
 from medunda.sources.cmems import VARIABLES
 from medunda.tools.argparse_utils import date_from_str
 from medunda.tools.layers import compute_layer_height
 from medunda.tools.logging_utils import configure_logger
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -34,12 +36,17 @@ VAR_METADATA = {
 }
 
 
+PLOTS=[
+    timeseries,
+    maps,
+]
+
+
 def parse_args ():
     """
     parse command line arguments: 
     --input-file: path of the input file
     --variable: name of the variable to plot
-    --mode: type of the plot that can be either 'time series' or 'maps'
     --output-dir: directory to save the download file
     """
     parser = argparse.ArgumentParser(
@@ -58,25 +65,24 @@ def parse_args ():
         required=True,
         help="Name of the variable to plot"
     )
-    parser.add_argument(
-        "--mode",
-        type=str,
-        choices=["timeseries", "maps"],
-        required=True,
-        help="Type of the plot that can be either 'time series' or 'maps' "
-    )
-    parser.add_argument(   
-        "--time",
-        type=date_from_str,
-        required=True,
-        help="The time of the plot"
-    )
     parser.add_argument(     
         "--output-dir",
         type=Path,
         default=Path("."),
         help="Directory where the downloaded files are saved",
     )
+
+    subparsers = parser.add_subparsers(
+        title="mode",
+        required=True,
+        dest="mode",
+        help="Sets which plot must be executed on the input file"
+    )
+
+    for mode in PLOTS:
+        LOGGER.debug("Letting module %s configure its parser", mode.__name__)
+        mode.configure_parser(subparsers)
+
     return parser.parse_args()
 
 
@@ -89,89 +95,31 @@ def check_variable (ds, var):
     return ds[var], VAR_METADATA[var]
 
 
-def compute_ch_integral (ds):
-
-    ds = xr.load_dataset(ds)
-
-    layer_height = compute_layer_height(ds.depth.values) 
-
-    chl = ds['chl'] 
-    chl_integrated = (chl * layer_height[:, np.newaxis, np.newaxis]).sum(dim='depth')
-
-    return chl_integrated
-
-
-def plot_timeseries (data:xr.DataArray, metadata: dict):
-
-    # Aggregate spatial dims by mean over lat and lon if they exist
-    spatial_dims = [dim for dim in ['lat', 'latitude', 'lon', 'longitude'] if dim in data.dims]
-    if spatial_dims:
-        time_series = data.mean(dim=spatial_dims)
-    else:
-        time_series = data  #has one dimension already 
-    
-    cmap= metadata['cmap']
-    if isinstance(cmap, str):
-        cmap=plt.get_cmap(cmap)
-
-    plt.figure(figsize=(10,5))
-    plt.plot(time_series['time'], time_series,)
-    plt.title(f"Time Series of {metadata['label']}")
-    plt.xlabel('Time')
-    plt.ylabel(f"{metadata['label']} [{metadata['unit']}]")
-    plt.show()
-
-
-def plot_maps (data: xr.DataArray, metadata: dict, time):
-    
-    # Select the time slice for mapping (default first time step)
-    if 'time' in data.dims:
-        data_slice = data.sel(time=time)
-    else:
-        data_slice = data
-
-    plt.figure(figsize=(8,6))
-    cmap = metadata['cmap']
-    # If cmap is a string (e.g. 'viridis'), convert to plt colormap
-    if isinstance(cmap, str):
-        cmap = plt.get_cmap(cmap)
-        
-    im = plt.imshow(data_slice, cmap=cmap)
-    plt.title(f"{metadata['label']}) at time {time}")
-    cbar = plt.colorbar(im)
-    cbar.set_label(metadata['unit'])
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.show()
-
-
-def extract_and_plot_layers(filepath: Path, variable: str, mode:str, time):
+def plotter (filepath: Path, variable: str, mode:str, args):
     """Extracts and plots surface, bottom, and average layers of the given variable."""
     
     with xr.open_dataset(filepath) as ds: 
         data_var, metadata = check_variable(ds, variable)
 
-        if mode == 'timeseries':
+        if mode == 'plotting_timeseries':
             if 'time' not in ds[variable].dims:
                 raise ValueError (f"Variable '{variable} does not have the time dimension")
+            else:  
+                timeseries.plotting_timeseries(
+                    data=data_var, 
+                    metadata=metadata,
+                    start_time=args.start_time,
+                    end_time=args.end_time
+                    )
         
-        if variable == 'chl': ######
-
-            """ this should be able to call for the function to 
-            compute the integral, pass it into a temp_file, 
-            then do the plots accordingly"""
-            
-            chl_integrated = compute_ch_integral(filepath)
-            plot_timeseries(chl_integrated, metadata)
-    
+        elif mode == "plotting_maps":
+            maps.plotting_maps(
+                data=data_var, 
+                metadata=metadata, 
+                time=args.time)
+        
         else: 
-            """for any variable other than chlorophyll"""
-            if mode == "timeseries": 
-                plot_timeseries(data_var, metadata)
-            elif mode == "maps":
-                plot_maps(data_var, metadata, time)
-            else: 
-                raise ValueError(f"Invalid mode")
+            raise ValueError(f"Invalid mode")
 
 
 def main ():
@@ -180,19 +128,17 @@ def main ():
     output_dir = args.output_dir
     variable = args.variable
     mode = args.mode
-    time = args.time
+    data_file = args.input_file 
 
-    if not output_dir.is_dir():
-        raise ValueError(f"The path '{output_dir}' does not exist or is not a directory.")
+    if not data_file.exists():
+        raise FileNotFoundError (f"The file '{data_file}' does not exist.")
 
-    data_file_list = list(output_dir.glob("*.nc"))
-    if not data_file_list:
-        raise FileNotFoundError(f"No NetCDF (.nc) files found in '{output_dir}'.")
+    if data_file.suffix != ".nc":
+        raise ValueError(f"Unexpected file.")
 
-    data_file = data_file_list[0]
     LOGGER.info(f"Selected file: {data_file.name}")
 
-    extract_and_plot_layers(filepath=data_file, variable=variable, mode=mode, time=time)
+    plotter (filepath=data_file, variable=variable, mode=mode, args=args)
 
     LOGGER.info(f"Plotting completed for variable '{variable}' in mode '{mode}'")
 
