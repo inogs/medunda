17652ba8279f3b19be36ff7276d02ff8000417e5
Nada Akkari <akkarinada8@gmail.com>
2025-12-16 17:57:55 +0100

    Refactor depth_average function to compute averages over multiple axis

--- src/medunda/actions/depth_average.py
+++ src/medunda/actions/depth_average.py
@@ -1,30 +1,77 @@
 import logging
 
+import numpy as np
 import xarray as xr
 
 from medunda.actions.average_between_layers import average_between_layers
 
 LOGGER = logging.getLogger(__name__)
-ACTION_NAME = "compute_depth_average"
+ACTION_NAME = "compute_average"
+
+AXIS = {
+    "depth": ["depth"],
+    "space": ["latitude", "longitude"],
+    "time": ["time"],
+}
 
 
 def configure_parser(subparsers):
-    subparsers.add_parser(
-        ACTION_NAME, help="Compute the average on all the vertical levels"
+    average_parser = subparsers.add_parser(
+        ACTION_NAME, help="Compute the average"
+    )
+    average_parser.add_argument(
+        "--axis",
+        type=str,
+        choices=["depth", "space", "time"],
+        nargs="+",
+        required=True,
+        help="Choose the axis on which the average will be computed.",
     )
 
 
-def compute_depth_average(data: xr.Dataset) -> xr.Dataset:
-    """Compute the average on all the vertical levels of the dataset.
+def get_volume():
+    pass
+
+
+def compute_depth_average(data: xr.Dataset, axis) -> xr.Dataset:
+    """Compute the average on a given axis.
 
     Args:
         data (xr.Dataset): Input dataset with depth as one of the dimensions.
     """
-    LOGGER.info("Computing depth average")
-    depth_min = data.depth.min().item()
-    depth_max = data.depth.max().item()
+    LOGGER.info(f"Computing average over axis '{axis}'")
 
-    depth_average = average_between_layers(data, depth_min - 1, depth_max + 1)
-    LOGGER.info("Depth average computed successfully")
+    if axis == "depth":
+        depth_min = data.depth.min().item()
+        depth_max = data.depth.max().item()
+        averaged_dataset = average_between_layers(
+            data, depth_min - 1, depth_max + 1
+        )
+        LOGGER.info("Depth average computed successfully")
 
-    return depth_average
+    elif axis == "space":
+        mask = np.ma.getmaskarray(data.to_masked_array(copy=False))[0, :, :, :]
+
+        weights = get_volume(data)
+        weights = weights * (~mask)
+
+        averaged_dataset = xr.Dataset()
+        for var in data.data_vars:
+            da = data[var]
+            weighted_sum = (da * weights).sum(dim=["latitude", "longitude"])
+            total_weights = weights.sum(dim=["latitude", "longitude"])
+            averaged_dataset[var] = weighted_sum / total_weights
+
+        LOGGER.info("Space average computed successfully")
+
+    elif axis == "time":
+        averaged_dataset = data.mean(dim="time")
+
+        LOGGER.info("Time average computed successfully")
+
+    else:
+        raise ValueError(
+            f"Axis '{axis}' is not valid. Choose from {list(AXIS.keys())}"
+        )
+
+    return averaged_dataset
