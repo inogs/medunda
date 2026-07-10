178ddfa64b2f90efcb183308272f2613ff4a7f8e
Stefano Piani <spiani@ogs.it>
2026-07-10 16:59:08 +0200

    Silenced a warning

--- src/medunda/__init__.py
+++ src/medunda/__init__.py
@@ -1,3 +1,16 @@
+import warnings
+
 from medunda.components.geodata import GeoDataCollection
+
+
+def _configure_warning_filters() -> None:
+    warnings.filterwarnings(
+        "ignore",
+        message=r"Setting the shape on a NumPy array has been deprecated in NumPy 2\.5\..*",
+        category=DeprecationWarning,
+    )
+
+
+_configure_warning_filters()
 
 __all__ = ["GeoDataCollection"]
